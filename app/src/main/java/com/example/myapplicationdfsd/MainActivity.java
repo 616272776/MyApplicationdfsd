a7bf3c9d7680c3d24d3678001a6f37cd33fc73ae
abc <abc@qq.com>
2021-05-25 12:31:15 +0800

    加了通话声音

--- app/src/main/java/com/example/myapplicationdfsd/MainActivity.java
+++ app/src/main/java/com/example/myapplicationdfsd/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.myapplicationdfsd;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import org.json.JSONObject;
 import org.webrtc.AudioSource;
 import org.webrtc.AudioTrack;
 import org.webrtc.Camera1Enumerator;
@@ -9,10 +14,12 @@ import org.webrtc.Camera2Enumerator;
 import org.webrtc.DefaultVideoDecoderFactory;
 import org.webrtc.DefaultVideoEncoderFactory;
 import org.webrtc.EglBase;
+import org.webrtc.IceCandidate;
 import org.webrtc.MediaConstraints;
 import org.webrtc.MediaStream;
 import org.webrtc.PeerConnection;
 import org.webrtc.PeerConnectionFactory;
+import org.webrtc.SessionDescription;
 import org.webrtc.SurfaceTextureHelper;
 import org.webrtc.SurfaceViewRenderer;
 import org.webrtc.VideoCapturer;
@@ -23,61 +30,214 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements SignalingClient.Callback {
 
+    EglBase.Context eglBaseContext;
+    PeerConnectionFactory peerConnectionFactory;
+    SurfaceViewRenderer localView;
+    MediaStream mediaStream;
+    List<PeerConnection.IceServer> iceServers;
+
+    HashMap<String, PeerConnection> peerConnectionMap;
+    SurfaceViewRenderer[] remoteViews;
+    int remoteViewsIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) {
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, 1);
+        }
+
+
+        peerConnectionMap = new HashMap<>();
+        iceServers = new ArrayList<>();
+        iceServers.add(PeerConnection.IceServer.builder("stun:stun.l.google.com:19302").createIceServer());
+
+        eglBaseContext = EglBase.create().getEglBaseContext();
+
         // create PeerConnectionFactory
-        PeerConnectionFactory.InitializationOptions initializationOptions =
-                PeerConnectionFactory.InitializationOptions.builder(this).createInitializationOptions();
-        PeerConnectionFactory.initialize(initializationOptions);
-        PeerConnectionFactory peerConnectionFactory = PeerConnectionFactory.builder().createPeerConnectionFactory();
-
-        // create AudioSource
-        AudioSource audioSource = peerConnectionFactory.createAudioSource(new MediaConstraints());
-        AudioTrack audioTrack = peerConnectionFactory.createAudioTrack("101", audioSource);
-
-        EglBase.Context eglBaseContext = EglBase.create().getEglBaseContext();
+        PeerConnectionFactory.initialize(PeerConnectionFactory.InitializationOptions
+                .builder(this)
+                .createInitializationOptions());
+        PeerConnectionFactory.Options options = new PeerConnectionFactory.Options();
+        DefaultVideoEncoderFactory defaultVideoEncoderFactory =
+                new DefaultVideoEncoderFactory(eglBaseContext, true, true);
+        DefaultVideoDecoderFactory defaultVideoDecoderFactory =
+                new DefaultVideoDecoderFactory(eglBaseContext);
+        peerConnectionFactory = PeerConnectionFactory.builder()
+                .setOptions(options)
+                .setVideoEncoderFactory(defaultVideoEncoderFactory)
+                .setVideoDecoderFactory(defaultVideoDecoderFactory)
+                .createPeerConnectionFactory();
 
         SurfaceTextureHelper surfaceTextureHelper = SurfaceTextureHelper.create("CaptureThread", eglBaseContext);
         // create VideoCapturer
-        VideoCapturer videoCapturer = createCameraCapturer();
+        VideoCapturer videoCapturer = createCameraCapturer(true);
         VideoSource videoSource = peerConnectionFactory.createVideoSource(videoCapturer.isScreencast());
         videoCapturer.initialize(surfaceTextureHelper, getApplicationContext(), videoSource.getCapturerObserver());
         videoCapturer.startCapture(480, 640, 30);
 
-//        SurfaceViewRenderer localView = findViewById(R.id.localView);
-//        localView.setMirror(true);
-//        localView.init(eglBaseContext, null);
-//
-//        // create VideoTrack
-//        VideoTrack videoTrack = peerConnectionFactory.createVideoTrack("101", videoSource);
+
+
+        localView = findViewById(R.id.localView);
+        localView.setMirror(true);
+        localView.init(eglBaseContext, null);
+
+        // create VideoTrack
+        VideoTrack videoTrack = peerConnectionFactory.createVideoTrack("100", videoSource);
+
+        MediaConstraints constraints = new MediaConstraints();
+        AudioSource audioSource = peerConnectionFactory.createAudioSource(constraints);
+        AudioTrack audioTrack = peerConnectionFactory.createAudioTrack("101", audioSource);
+
 //        // display in localView
-//        videoTrack.addSink(localView);
+        videoTrack.addSink(localView);
+
+
+        remoteViews = new SurfaceViewRenderer[]{
+                findViewById(R.id.remoteView),
+                findViewById(R.id.remoteView2),
+                findViewById(R.id.remoteView3),
+        };
+        for (SurfaceViewRenderer remoteView : remoteViews) {
+            remoteView.setMirror(false);
+            remoteView.init(eglBaseContext, null);
+        }
+
+
+        mediaStream = peerConnectionFactory.createLocalMediaStream("mediaStream");
+        mediaStream.addTrack(videoTrack);
+        mediaStream.addTrack(audioTrack);
+
+        SignalingClient.get().init(this);
+
+
+
     }
 
-    private VideoCapturer createCameraCapturer() {
+
+    private synchronized PeerConnection getOrCreatePeerConnection(String socketId) {
+        PeerConnection peerConnection = peerConnectionMap.get(socketId);
+        if (peerConnection != null) {
+            return peerConnection;
+        }
+        peerConnection = peerConnectionFactory.createPeerConnection(iceServers, new PeerConnectionAdapter("PC:" + socketId) {
+            @Override
+            public void onIceCandidate(IceCandidate iceCandidate) {
+                super.onIceCandidate(iceCandidate);
+                SignalingClient.get().sendIceCandidate(iceCandidate, socketId);
+            }
+
+            @Override
+            public void onAddStream(MediaStream mediaStream) {
+                super.onAddStream(mediaStream);
+                VideoTrack remoteVideoTrack = mediaStream.videoTracks.get(0);
+                runOnUiThread(() -> {
+                    remoteVideoTrack.addSink(remoteViews[remoteViewsIndex++]);
+                });
+            }
+        });
+        peerConnection.addStream(mediaStream);
+        peerConnectionMap.put(socketId, peerConnection);
+        return peerConnection;
+    }
+
+    @Override
+    public void onCreateRoom() {
+
+    }
+
+    @Override
+    public void onPeerJoined(String socketId) {
+        PeerConnection peerConnection = getOrCreatePeerConnection(socketId);
+        peerConnection.createOffer(new SdpAdapter("createOfferSdp:" + socketId) {
+            @Override
+            public void onCreateSuccess(SessionDescription sessionDescription) {
+                super.onCreateSuccess(sessionDescription);
+                peerConnection.setLocalDescription(new SdpAdapter("setLocalSdp:" + socketId), sessionDescription);
+                SignalingClient.get().sendSessionDescription(sessionDescription, socketId);
+            }
+        }, new MediaConstraints());
+    }
+
+    @Override
+    public void onSelfJoined() {
+
+    }
+
+    @Override
+    public void onPeerLeave(String msg) {
+
+    }
+
+    @Override
+    public void onOfferReceived(JSONObject data) {
+        runOnUiThread(() -> {
+            final String socketId = data.optString("from");
+            PeerConnection peerConnection = getOrCreatePeerConnection(socketId);
+            peerConnection.setRemoteDescription(new SdpAdapter("setRemoteSdp:" + socketId),
+                    new SessionDescription(SessionDescription.Type.OFFER, data.optString("sdp")));
+            peerConnection.createAnswer(new SdpAdapter("localAnswerSdp") {
+                @Override
+                public void onCreateSuccess(SessionDescription sdp) {
+                    super.onCreateSuccess(sdp);
+                    peerConnectionMap.get(socketId).setLocalDescription(new SdpAdapter("setLocalSdp:" + socketId), sdp);
+                    SignalingClient.get().sendSessionDescription(sdp, socketId);
+                }
+            }, new MediaConstraints());
+
+        });
+    }
+
+    @Override
+    public void onAnswerReceived(JSONObject data) {
+        String socketId = data.optString("from");
+        PeerConnection peerConnection = getOrCreatePeerConnection(socketId);
+        peerConnection.setRemoteDescription(new SdpAdapter("setRemoteSdp:" + socketId),
+                new SessionDescription(SessionDescription.Type.ANSWER, data.optString("sdp")));
+    }
+
+    @Override
+    public void onIceCandidateReceived(JSONObject data) {
+        String socketId = data.optString("from");
+        PeerConnection peerConnection = getOrCreatePeerConnection(socketId);
+        peerConnection.addIceCandidate(new IceCandidate(
+                data.optString("id"),
+                data.optInt("label"),
+                data.optString("candidate")
+        ));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SignalingClient.get().destroy();
+    }
+
+    private VideoCapturer createCameraCapturer(boolean isFront) {
         Camera1Enumerator enumerator = new Camera1Enumerator(false);
         final String[] deviceNames = enumerator.getDeviceNames();
 
         // First, try to find front facing camera
         for (String deviceName : deviceNames) {
-            if (enumerator.isFrontFacing(deviceName)) {
-                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
-
-                if (videoCapturer != null) {
-                    return videoCapturer;
-                }
-            }
-        }
-
-        // Front facing camera not found, try something else
-        for (String deviceName : deviceNames) {
-            if (!enumerator.isFrontFacing(deviceName)) {
+            if (isFront ? enumerator.isFrontFacing(deviceName) : enumerator.isBackFacing(deviceName)) {
                 VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
 
                 if (videoCapturer != null) {
@@ -88,5 +248,4 @@ public class MainActivity extends AppCompatActivity{
 
         return null;
     }
-
 }
